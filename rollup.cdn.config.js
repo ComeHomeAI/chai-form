9b416f2351b0b5b40585874cacd1368e123f852e
Lars Kemmann <lars@comehome.ai>
2024-04-12 22:13:38 -0400

    Added a production-oriented Rollup config & publish script

---
+++ rollup.cdn.config.js
@@ -0,0 +1,44 @@
+/**
+ * @license
+ * Copyright 2018 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+import summary from 'rollup-plugin-summary';
+import terser from '@rollup/plugin-terser';
+//import minifyHTML from 'rollup-plugin-minify-html-literals';
+import resolve from '@rollup/plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
+
+export default {
+  input: 'chai-form.js',
+  output: {
+    file: 'dist/chai-form.bundled.min.js',
+    format: 'esm',
+  },
+  onwarn(warning) {
+    if (warning.code !== 'THIS_IS_UNDEFINED') {
+      console.error(`(!) ${warning.message}`);
+    }
+  },
+  plugins: [
+    replace({'Reflect.decorate': 'undefined'}),
+    resolve(),
+    /**
+     * This minification setup is optimized for a standalone
+     * production output (e.g., for CDN hosting).
+     */
+    //minifyHTML(),
+    terser({
+      ecma: 2021,
+      module: true,
+      warnings: true,
+      mangle: {
+        properties: {
+          regex: /^__/,
+        },
+      },
+    }),
+    summary(),
+  ],
+};
