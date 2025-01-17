04ec3e04fc593ca6fc61da9028418fcd3bdd78e5
Christopher Timm <christopher@timmch.de>
2025-01-16 22:55:41 -0500

    Remove checkbox and make tcpa-agreement text-only upon submission

--- src/chai-tcpa-agreement.ts
+++ src/chai-tcpa-agreement.ts
@@ -1,9 +1,8 @@
-import {ChaiFieldBase} from './ChaiFieldBase';
-import {css, html} from 'lit';
+import {css, html, LitElement} from 'lit';
 import {customElement, property, state} from 'lit/decorators.js';
 
 @customElement('chai-tcpa-agreement')
-export class ChaiTcpaAgreement extends ChaiFieldBase<boolean> {
+export class ChaiTcpaAgreement extends LitElement {
   static override styles = css`
       label {
           visibility: hidden;
@@ -13,15 +12,9 @@ export class ChaiTcpaAgreement extends ChaiFieldBase<boolean> {
       p {
           font-size: calc(var(--chai-form-font-size) * 0.8);
       }
-
   `;
-
-  constructor() {
-    super('tcpa-agreement', '', '');
-  }
-
   @property()
-  accessor text = 'I consent to receive calls and texts at this number about my move, including marketing by autodialer and prerecorded and artificial voice and email, but not as a condition of any purchase. I also agree to the Terms of Use, and to the Privacy Policy regarding the information relating to me.';
+  accessor text = 'By submitting this form I consent to receive calls and texts at this number about my move, including marketing by autodialer and prerecorded and artificial voice and email, but not as a condition of any purchase. I also agree to the Terms of Use, and to the Privacy Policy regarding the information relating to me.';
 
   @property()
   accessor moreText = 'Msg/data rates may apply. This consent applies even if I am on a corporate, state or national Do Not Call list.\n';
@@ -29,32 +22,14 @@ export class ChaiTcpaAgreement extends ChaiFieldBase<boolean> {
   @state()
   private showMoreText = false;
 
-  protected deserializeValue(storedValue: string | null): boolean {
-    return storedValue === 'true';
-  }
-
-  protected isValueSet(): boolean {
-    return true;
-  }
-
-  protected isValueValid(): boolean {
-    return true;
-  }
-
-  private checked() {
-    this.value = !this.value;
-    this.updateField(this.value);
-  }
-
   private toggleMoreText() {
     this.showMoreText = !this.showMoreText;
   }
 
 
-  protected override renderInput() {
+  protected override render() {
     return html`
       <p>
-        <input id="${this._fieldId}" name="tcpa-agreement" type="checkbox" .checked="${this.value}" @click="${this.checked}" />
         ${this.text}
         <a href="javascript:void(0)" @click="${this.toggleMoreText}">
           ${this.showMoreText ? '' : 'More'}
@@ -62,10 +37,6 @@ export class ChaiTcpaAgreement extends ChaiFieldBase<boolean> {
         ${this.showMoreText ? html`<span>${this.moreText}</span>` : ''}
       </p>
     `;
-  }
-
-  protected serializeValue(value: boolean): string {
-    return value.toString();
   }
 }
 
